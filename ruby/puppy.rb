1d68d64359dd4fbac920752b7f6c29d71ff7d370
Jeffery C. Brown <jeffery.c.brown@gmail.com>
2016-04-27 00:51:41 -0700

    6.1 features

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# PUPPY ------------------------
+# Shaggyshus Canineous
+
+# CHARACTERISTICS ----------------
+# Hair length: varies
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Roll
+# Squat
