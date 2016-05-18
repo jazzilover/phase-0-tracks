42f2303a7e7f4f95e4e467cf012834027a0c35dc
Jeffery Brown <jeffery.c.brown@gmail.com>
2016-05-18 01:56:51 -0700

    create gps2.2

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -41,9 +41,7 @@
 # 	puts pretty_list
 # end
 
-# # ###############
-# # # DRIVER CODE #
-# # ###############
+
 
 # grocery_list("carrots apples cereal pizza")
 
@@ -56,73 +54,90 @@
 # output: hash
 
 
-list = {}
 
-def grocery_list(hash, items)
+
+def create_list(list_hash, items)
 	grocery_list = items.split
 	grocery_list.each do |item|
-		hash.store(item, 1)
-	end	
-	hash
+		list_hash.store(item, 1)
+		end
+	p "you created a list"
+	print_list(list_hash)		
+	# list_hash
 end	
-
-grocery_list(list, "carrots apples ice cream")
-
 
 # Method to add an item to a list
 # input: item name and optional quantity
 # steps: add item and quantity to hash
 # output: print new list with updated hash
 
-def add_item(hash, item, quantity)
-	hash.store(item, quantity)
-	hash
+def add_item(add_hash, item, quantity)
+	add_hash.store(item, quantity)
+	p "you added an item"
+	print_list (add_hash)
 end
-
-
 
 # Method to remove an item from the list
 # input: item name
 # steps: check for item in list, then remove from list
 # output: print new list with updated hash
 
-def remove_item(hash, item)
-	hash.delete(item)
-	hash
+def remove_item(remove_hash, item)
+	remove_hash.delete(item)
+	p "you removed an item"
+	print_list(remove_hash)
 end	
 
-# Method to update the quantity of an item
-# input: new quantity of item
-# steps: check hash for quantity of particular item, update quantity
-# output: print new list with updated hash to verify change is made
+# # Method to update the quantity of an item
+# # input: new quantity of item
+# # steps: check hash for quantity of particular item, update quantity
+# # output: print new list with updated hash to verify change is made
 
-def update_quantity(hash, item, quantity)
+def update_quantity(update_hash, item, quantity)
 	new_hash = {item => quantity}
-	hash.merge!(new_hash)
-	hash
+	update_hash.merge!(new_hash)
+	p "you updated your quantity"
+	print_list(update_hash)
 end	
-	
 
-# Method to print a list and make it look pretty
-# input: name of grocery hash
-# steps: print out list of items and quantity
-# output: grocery list hash
+# # Method to print a list and make it look pretty
+# # input: name of grocery hash
+# # steps: print out list of items and quantity
+# # output: grocery list hash
 
-def print_list(hash)
-	hash.each do |item, quantity|
+def print_list(print_hash)
+	print_hash.each do |item, quantity|
 		puts "Your Grocery list: consitss of the following item #{item}, and it has a quantity of #{quantity}"
 	end
 end	
 
-grocery_list(list, "carrots apples cereal pizza")
+# # ###############
+# # # DRIVER CODE #
+# # ###############
+
+list = {}
+# create_list(list, "carrots apples ice_cream")
+create_list(list, "carrots apples cereal pizza")
 p list
 add_item(list, "lemonade", 2)
 add_item(list, "tomatoes", 3)
 add_item(list, "onions", 1)
-add_item(list, "ice cream", 4)
+add_item(list, "ice_cream", 4)
 p list
 remove_item(list, "lemonade")
 p list
-update_quantity(list, "ice cream", 1)
+update_quantity(list, "ice_cream", 1)
 p list
 print_list(list)
+
+
+
+
+
+
+	
+
+
+
+
+
